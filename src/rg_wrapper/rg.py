6eb35fd9690dceb0da5fdbbb5462038bad3daaa1
KhoalaS <s4khleee@uni-trier.de>
2025-04-13 15:08:34 +0200

    add logging to rg wrapper

--- src/rg_wrapper/rg.py
+++ src/rg_wrapper/rg.py
@@ -9,6 +9,10 @@ def gen_candidates(working_dir: str = os.getcwd()) -> tuple[list[str], int]:
                         capture_output=True,
                         cwd=working_dir)
 
+    if p1.returncode != 0:
+        print(
+            f'error during gen_candidates phase 1: {p1.returncode, p1.stdout}')
+
     p2 = subprocess.run(
         ["rg", "-v",
          "kotlinx|androidx|okhttp|bitdrift|airbnb|google|facebook"],
@@ -16,5 +20,9 @@ def gen_candidates(working_dir: str = os.getcwd()) -> tuple[list[str], int]:
         capture_output=True,
         text=True,
         cwd=working_dir)
+
+    if p2.returncode != 0:
+        print(
+            f'error during gen_candidates phase 2: {p2.returncode, p2.stdout}')
 
     return p2.stdout.split('\n')[:-1], p2.returncode
