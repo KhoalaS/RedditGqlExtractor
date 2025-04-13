bd394d0a4e1b840394c90e30cd20a1d58a04298e
KhoalaS <s4khleee@uni-trier.de>
2025-04-13 15:25:13 +0200

    try using full path to rg on linux

--- src/rg_wrapper/rg.py
+++ src/rg_wrapper/rg.py
@@ -1,10 +1,20 @@
 import subprocess
+import platform
 import os
+import shutil
 
 
 def gen_candidates(working_dir: str = os.getcwd()) -> tuple[list[str], int]:
 
-    p1 = subprocess.run(["rg", "-l", "method public (final)? hashCode"],
+    os_name = platform.uname().system
+    program_name = 'rg'
+    if os_name == 'Linux':
+        rg_path = shutil.which('rg')
+        if rg_path is not None:
+            program_name = rg_path
+
+    p1 = subprocess.run([program_name,
+                         "-l", "method public (final)? hashCode"],
                         text=True,
                         capture_output=True,
                         cwd=working_dir)
@@ -14,7 +24,7 @@ def gen_candidates(working_dir: str = os.getcwd()) -> tuple[list[str], int]:
             f'error during gen_candidates phase 1: {p1.stdout, p1.stderr}')
 
     p2 = subprocess.run(
-        ["rg", "-v",
+        [program_name, "-v",
          "kotlinx|androidx|okhttp|bitdrift|airbnb|google|facebook"],
         input=p1.stdout,
         capture_output=True,
