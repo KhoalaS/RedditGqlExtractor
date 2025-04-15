66ea733be9c52eacd902fcfdd1964ec6b379d47d
KhoalaS <s4khleee@uni-trier.de>
2025-04-15 22:02:12 +0200

    fix test

--- src/__tests__/candidates_test.py
+++ src/__tests__/candidates_test.py
@@ -1,10 +1,17 @@
 import unittest
 from utils import candidates
+import os
 
 
 class CandidatesTest(unittest.TestCase):
     def test_find_smali_files(self):
         self.assertEqual(
             set([file for file in candidates.get_candidates()]),
-            set(['.\\src\\__tests__\\test_data\\example.smali',
-                '.\\src\\__tests__\\test_data\\example_2.smali']))
+            set([
+                os.path.join('.', 'src', '__tests__',
+                             'test_data', 'example.smali'),
+                os.path.join('.', 'src', '__tests__',
+                             'test_data', 'example_2.smali'),
+                os.path.join('.', 'src', '__tests__',
+                             'test_data', 'example_3.smali'),
+            ]))
