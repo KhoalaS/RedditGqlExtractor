42c528f6279b8012602a4199888f411d5e467c34
KhoalaS <khoalekappa@gmail.com>
2025-04-19 01:58:09 +0200

    rename enum candidate file

--- scripts/gen_candidates.sh
+++ scripts/gen_candidates.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rg -l 'method public (final )?hashCode' | rg -v 'kotlinx|androidx|okhttp|bitdrift|airbnb|google|facebook' > candidates.txt
-rg -l '\.class public final enum Lcom/reddit' > enum_candidates.txt
+rg -l '\.class public final enum Lcom/reddit' > enums.txt
